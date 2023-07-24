f3851cb8033d18933e793e0880aaec2737997bc7
WhiteHanClean <whitehanclean@gmail.com>
2023-07-24 13:44:59 +0600

    production

---
+++ components/Info/Info.jsx
@@ -0,0 +1,54 @@
+﻿import React from 'react';
+import s from './Info.module.scss'
+
+const Info = () => {
+    return (
+        <>
+            <div className={s.container}>
+                <div className={s.info_block}>
+                    <div className={s.first}>
+                        <div className={s.one}>
+                            <li> <img src="/transport.png" alt="" /> Доставка авиа и <br />
+                                автомобильным транспортом</li>
+                            <li> <img src="/map.png" alt="" /> Безопасные и оптимальные маршруты <br /> с минимальной стоимостью</li>
+                            <li> <img src="/money.png" alt="" /> Оплата доставки после получения. <br /> Возможна отсрочка</li>
+                        </div>
+                        <div className={s.two}>
+                            <iframe width="660" height="387" src="https://www.youtube.com/embed/johcE5s525M?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                        </div>
+                    </div>
+                    <div className={s.second}>
+                        <h1>Antik Kargo</h1>
+                        <h3>доставка товаров из <br /> Турции с минимальным <br /> участием с вашей <br /> стороны</h3>
+                        <div className={s.video}>
+                            <iframe width="660" height="387" src="https://www.youtube.com/embed/johcE5s525M?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                        </div>
+                        <p>
+                            Ищите компанию, которая организует <br /> доставку грузов из Турции в Россию с <br /> минимальным вмешательством с вашей <br /> стороны?
+                            <br />
+                            <br />
+                            Вы попали точно по адресу! Мы возьмем на <br /> себя все заботы об отправке, доставке, <br /> таможенном оформлении и сохранности <br /> вашего груза.
+                            <br />
+                            <br />
+                            Огромный опыт и собственная <br /> инфраструктура позволяют нам выстроить <br /> маршруты доставки с лучшей ценой на <br /> рынке, а также гарантировать сохранность <br /> грузов и соблюдение сроков доставки.
+                        </p>
+                        <div className={s.under_block}>
+                            <h1>
+                                Работаем
+                                <br />
+                                с 2000 года
+                            </h1>
+                            <h1>
+                                12 000 довольных
+                                <br />
+                                клиентов
+                            </h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Info;
---
+++ components/Pirce/Price.jsx
@@ -0,0 +1,56 @@
+﻿import React from 'react';
+import s from './Price.module.scss'
+import { delivery_price } from '@/constants/constants';
+import ParticlesBackground from '../Particles/Particles';
+
+const Price = () => {
+    return (
+        <>
+            <div className={s.background}>
+                <div className={s.particle}>
+                    <ParticlesBackground />
+                </div>
+                <div className={s.container}>
+                    <div className={s.price}>
+                        <div className={s.title}>
+                            <h1>стоимость</h1>
+                            <p>доставки товаров из Турции в <span>Иркутск</span></p>
+                            <div className={s.important_block}>
+                                <img src="/important.png" alt="" />
+                                <h3>
+                                    <span>Прямая доставка в г. Ухрюпинск не осуществляется.</span> <br />
+                                    Мы доставим товар в Москву, затем отправим его в ваш город любой транспортной компанией.
+                                </h3>
+                            </div>
+                            <div className={s.tovar}>
+
+                            </div>
+                        </div>
+                        <div className={s.content}>
+                            {delivery_price.map((del) => (
+                                <div id={del.id} className={s.block}>
+                                    <div className={s.first}>
+                                        <p>{del.tarif}</p>
+                                        <h1>{del.title}</h1>
+                                        <div>
+                                            <li> <img src={del.img} alt="img" /> {del.transport}</li>
+                                            <li> <img src={del.img2} alt="img" /> {del.transport2}</li>
+                                        </div>
+                                        <h3>{del.delivery}</h3>
+                                    </div>
+                                    <div className={s.second}>
+                                        <h1>{del.price}</h1>
+                                        <p>Цена за <br /> доставку <br /> текстиля</p>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                        <button>Связаться с нами</button>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Price;
